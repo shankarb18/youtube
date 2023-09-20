9a7cb2b22a5112f8e165741054ef92b00919a815
shankarb18 <shankarbhore7993@gmail.com>
2023-09-20 23:42:01 +0530

    youtube Api added

--- src/components/VideoContainer.js
+++ src/components/VideoContainer.js
@@ -13,6 +13,7 @@ const VideoContainer = () => {
   const getVideos = async () => {
     const data = await fetch(YOUTUBE_VIDEOS_API);
     const json = await data.json();
+    console.log(json);
     setVideos(json.items);
   };
 
