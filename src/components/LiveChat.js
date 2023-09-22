5e5d8c1ecf2f498467efb7aa33d02dc29911ba88
shankarb18 <shankarbhore7993@gmail.com>
2023-09-22 20:40:17 +0530

    changed the api

--- src/components/LiveChat.js
+++ src/components/LiveChat.js
@@ -45,7 +45,7 @@ const LiveChat = () => {
 
           dispatch(
             addMessage({
-              name: "Akshay Saini",
+              name: "Shankar Bhore",
               message: liveMessage,
             })
           );
